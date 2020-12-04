8d8de8e41142dd1cf97f67b2c22cd7596b644f31
Kayla Branch <KBranch@rightpoint.com>
2020-12-03 21:23:53 -0700

    day 1 exercise 2

---
+++ day1/exercise2.js
@@ -0,0 +1,33 @@
+import {input} from './input.js';
+import {TextListToArray} from '../data-loaders/TextListToArray.js';
+
+let data = TextListToArray(input);
+
+// Find the THREE entries that sum to 2020 and then multiply those two numbers together
+
+// PSEUDOCODE:
+// for each, minus # from 2020
+// then loop again, one off from first loop index, and minus THAT # from previous subtraction value
+// then look for remainder in the rest of the array
+// if found, multiply and return
+// else continue
+
+// SOLUTION:
+function multipleOf2020SumOfThree() {
+    data.some(num => {
+        let difference = 2020 - parseInt(num);
+
+        // slice does not modify the original data
+        let found = data.slice(1).some(secondNum => {
+            let newDifference = difference - parseInt(secondNum);
+
+            if (data.includes(newDifference.toString())) {
+                return newDifference * num * secondNum;
+            }
+        });
+        // makes sure to exit outer loop as soon as answer is found, prevents extra work
+        return found;
+    });
+}
+
+multipleOf2020SumOfThree();
